02c26d84c0d67106e085051830abbd6db8020ceb
Kak_i7 <jordi.z80@gmail.com>
2023-06-26 02:52:50 +0200

    fixed mixed tabs-spaces

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,24 @@
 // Note: On V2 I could have used a background service to do the redirect, but it seems impossible
 // to do in V3. So it's possible a flicker is seen when the page loads before the redirect actually happens.
 
-    const url = new URL(window.location.href);
-    const hostname = url.hostname;
-    const pathname = url.pathname;
+	const url = new URL(window.location.href);
+	const hostname = url.hostname;
+	const pathname = url.pathname;
 
-    // Check if it's a Google domain and a search page
-    if (hostname.endsWith('google.co.uk') || (/\.google\.\w+$/).test(hostname)) 
+	// Check if it's a Google domain and a search page
+	if (hostname.endsWith('google.co.uk') || (/\.google\.\w+$/).test(hostname)) 
 	{
-        if (pathname === '/search') 
+		if (pathname === '/search') 
 		{
-            // Check if the parameters are already set to avoid infinite loop
-            const hasLanguageRestrict = url.searchParams.get('lr') === 'lang_ca';
+			// Check if the parameters are already set to avoid infinite loop
+			const hasLanguageRestrict = url.searchParams.get('lr') === 'lang_ca';
 
-            if (!hasLanguageRestrict) 
+			if (!hasLanguageRestrict) 
 			{
 				updateParameters (url);
-            }
-        }
-    }
+			}
+		}
+	}
 
 	function updateParameters (url)
 	{
